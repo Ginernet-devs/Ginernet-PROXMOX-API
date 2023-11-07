91f7771bf906601d4ad4382ac73d2680385825e6
Xavi <email@javier-rodriguez.com>
2023-11-07 20:52:26 +0100

    Fix: Auth failed

--- src/GClient.php
+++ src/GClient.php
@@ -34,6 +34,7 @@ class GClient
         try {
             $auth = new Login($this->connection, null);
             $result = $auth();
+            if (is_null($result))  return new AuthFailedException();
             $this->cookiesPVE = new CookiesPVE($result->getCSRFPreventionToken(), $result->getCookies(), $result->getTicket());
             return $result;
         } catch (AuthFailedException $ex) {
