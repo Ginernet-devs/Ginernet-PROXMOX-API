26cb9b0a575b60133724a43aab98839d04d94d8e
Jose Alberto Llucia <llucia1@hotmail.com>
2025-01-28 10:43:08 +0100

    FIX - Sonar

--- src/VM/App/Service/Help/So/Linux/CreateDataForLinuxVM.php
+++ src/VM/App/Service/Help/So/Linux/CreateDataForLinuxVM.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Ginernet\Proxmox\VM\App\Service\Help\So\Linux;
@@ -28,105 +27,24 @@ use Ginernet\Proxmox\VM\Domain\Model\UserModel;
 
 
 final class CreateDataForLinuxVM implements IBuildVMData
-
 {
-
-    private string $nodeName;
-    private int $vmId;
-    private ?int $vmCpuCores;
-    private ?string $vmName;
-    private ?int $vmNetId;
-    private ?string $vmNetModel;
-    private ?string $vmNetBridge;
-    private ?int $vmNetFirewall;
-    private ?bool $vmOnBoot;
-    private ?string $vmScsiHw;
-    private ?string $vmDiskType;
-    private ?int    $vmDiskId;
-    private ?string $vmDiskStorage;
-    private ?string $vmDiskDiscard;
-    private ?string $vmDiskCache;
-    private ?string $vmDiskImportFrom;
-    private ?string $vmTags;
-    private ?int    $vmCloudInitIdeId;
-    private ?string $vmCloudInitStorage;
-    private ?string $vmBootOrder;
-    private ?int $vmAgent;
-    private ?int    $vmNetNetId;
-    private ?string $vmNetIp;
-    private ?string $vmNetGw;
-    private ?string $vmOsUserName;
-    private ?string $vmOsPassword;
-    private ?string $vmCpuType;
-    private ?int $vmMemory = null;
-    private ?int $vmMemoryBallon = null;
-    private ?string $vmOsType = null;
-    private ?string $vmBios = null;
-    private ?string $vmMachinePc = null;
-    private ?string $vmEfiStorage = null;
-    private ?int $vmEfiKey = null;
-    private ?string $efidisckNvme = null;
-    private ?string $efidisckEnrroled = null;
-    private ?string $tpmstateNvme = null;
-    private ?string $tpmstateVersion = null;
 
 
 
     public function __construct(
-                                    string  $nodeName, int $vmId, ?int $vmCpuCores, ?string $vmName, ?int $vmNetId,
-                                    ?string $vmNetModel, ?string $vmNetBridge, ?int $vmNetFirewall, ?bool $vmOnBoot,
-                                    ?string $vmScsiHw, ?string $vmDiskType, ?int    $vmDiskId, ?string $vmDiskStorage,
-                                    ?string $vmDiskDiscard, ?string $vmDiskCache, ?string $vmDiskImportFrom, ?string $vmTags,
-                                    ?int    $vmCloudInitIdeId, ?string $vmCloudInitStorage, ?string $vmBootOrder, ?int $vmAgent,
-                                    ?int    $vmNetNetId, ?string $vmNetIp, ?string $vmNetGw, ?string $vmOsUserName,
-                                    ?string $vmOsPassword, ?string $vmCpuType, ?int $vmMemory = null, ?int $vmMemoryBallon = null,
-                                    ?string $vmOsType = null,?string $vmBios = null,?string $vmMachinePc = null,
-                                    ?string $vmEfiStorage = null, ?int $vmEfiKey = null,
-                                    ?string $efidisckNvme = null, ?string $efidisckEnrroled = null,
-                                    ?string $tpmstateNvme = null, ?string $tpmstateVersion = null
-                                )
-
+                                    private string  $nodeName, private int $vmId, private ?int $vmCpuCores, private ?string $vmName, private ?int $vmNetId,  // NOSONAR
+                                    private ?string $vmNetModel, private ?string $vmNetBridge, private ?int $vmNetFirewall, private ?bool $vmOnBoot, // NOSONAR
+                                    private ?string $vmScsiHw, private ?string $vmDiskType, private ?int    $vmDiskId, private ?string $vmDiskStorage, // NOSONAR
+                                    private ?string $vmDiskDiscard, private ?string $vmDiskCache, private ?string $vmDiskImportFrom, private ?string $vmTags, // NOSONAR
+                                    private ?int    $vmCloudInitIdeId, private ?string $vmCloudInitStorage, private ?string $vmBootOrder, private ?int $vmAgent, // NOSONAR
+                                    private ?int    $vmNetNetId, private ?string $vmNetIp, private ?string $vmNetGw, private ?string $vmOsUserName, // NOSONAR
+                                    private ?string $vmOsPassword, private ?string $vmCpuType, private ?int $vmMemory = null, private ?int $vmMemoryBallon = null, // NOSONAR
+                                    private ?string $vmOsType = null, private ?string $vmBios = null,private ?string $vmMachinePc = null, // NOSONAR
+                                    private ?string $vmEfiStorage = null, private ?int $vmEfiKey = null, // NOSONAR
+                                    private ?string $efidisckNvme = null, private ?string $efidisckEnrroled = null, // NOSONAR
+                                    private ?string $tpmstateNvme = null, private ?string $tpmstateVersion = null // NOSONAR
+                                ) // NOSONAR
     {
-        $this->nodeName = $nodeName;
-        $this->vmId = $vmId;
-        $this->vmCpuCores = $vmCpuCores;
-        $this->vmName = $vmName;
-        $this->vmNetId = $vmNetId;
-        $this->vmNetModel = $vmNetModel;
-        $this->vmNetBridge = $vmNetBridge;
-        $this->vmNetFirewall = $vmNetFirewall;
-        $this->vmOnBoot = $vmOnBoot;
-        $this->vmScsiHw = $vmScsiHw;
-        $this->vmDiskType = $vmDiskType;
-        $this->vmDiskId = $vmDiskId;
-        $this->vmDiskStorage = $vmDiskStorage;
-        $this->vmDiskDiscard = $vmDiskDiscard;
-        $this->vmDiskCache = $vmDiskCache;
-        $this->vmDiskImportFrom = $vmDiskImportFrom;
-        $this->vmTags = $vmTags;
-        $this->vmCloudInitIdeId = $vmCloudInitIdeId;
-        $this->vmCloudInitStorage = $vmCloudInitStorage;
-        $this->vmBootOrder = $vmBootOrder;
-        $this->vmAgent = $vmAgent;
-        $this->vmNetNetId = $vmNetNetId;
-        $this->vmNetIp = $vmNetIp;
-        $this->vmNetGw = $vmNetGw;
-        $this->vmOsUserName = $vmOsUserName;
-        $this->vmOsPassword = $vmOsPassword;
-        $this->vmCpuType = $vmCpuType;
-        $this->vmMemory = $vmMemory;
-        $this->vmMemoryBallon = $vmMemoryBallon;
-        $this->vmOsType = $vmOsType;
-        $this->vmBios = $vmBios;
-        $this->vmMachinePc = $vmMachinePc;
-        $this->vmEfiStorage = $vmEfiStorage;
-        $this->vmEfiKey = $vmEfiKey;
-        $this->efidisckNvme = $efidisckNvme;
-        $this->efidisckEnrroled = $efidisckEnrroled;
-        $this->tpmstateNvme = $tpmstateNvme;
-        $this->tpmstateVersion = $tpmstateVersion;
-                                                                                                                        
-        
 
     }
     
@@ -137,14 +55,16 @@ final class CreateDataForLinuxVM implements IBuildVMData
         $net= new NetModel($this->vmNetId, $this->vmNetModel, $this->vmNetBridge, $this->vmNetFirewall);
             
         $scsi= null;
-        if (strtolower($this->vmDiskType) == strtolower(DiskTypePVE::SCSI)) $scsi = new ScsiModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        if (strtolower($this->vmDiskType) == strtolower(DiskTypePVE::SCSI)) {
+            $scsi = new ScsiModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        }
         
         $ide= new IdeModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
  
         
-        $sata=new SataModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        $sata=new SataModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom ); // NOSONAR
 
-        $virtio= new VirtioModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        $virtio= new VirtioModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom ); // NOSONAR
 
         $ip = new IpModel($this->vmNetNetId,$this->vmNetIp,$this->vmNetGw);
 
@@ -156,13 +76,13 @@ final class CreateDataForLinuxVM implements IBuildVMData
 
         $cpu = new CpuModel($this->vmCpuType, $this->vmCpuCores, $this->vmMemory, $this->vmMemoryBallon);
 
-        $efi= !is_null($this->vmEfiKey)? new EfiModel($this->vmEfiStorage, $this->vmEfiKey) : null;
+        $efi= !is_null($this->vmEfiKey)? new EfiModel($this->vmEfiStorage, $this->vmEfiKey) : null; // NOSONAR
 
-        $machinePc= !is_null($this->vmMachinePc)? new MachineModel($this->vmMachinePc, $this->vmNetModel) : null;
+        $machinePc= !is_null($this->vmMachinePc)? new MachineModel($this->vmMachinePc, $this->vmNetModel) : null; // NOSONAR
 
-        $tpmstate= !is_null($this->tpmstateVersion)? new TpmstateModel(0, $this->tpmstateNvme, null, null, null, $this->tpmstateVersion) : null;
+        $tpmstate= !is_null($this->tpmstateVersion)? new TpmstateModel(0, $this->tpmstateNvme, null, null, null, $this->tpmstateVersion) : null; // NOSONAR
 
-        $efiDisck= !is_null($this->efidisckNvme)? new EfidisckModel(0, null, $this->efidisckNvme, null, null, $this->efidisckEnrroled) : null;
+        $efiDisck= !is_null($this->efidisckNvme)? new EfidisckModel(0, null, $this->efidisckNvme, null, null, $this->efidisckEnrroled) : null; // NOSONAR
 
 
 
--- src/VM/App/Service/Help/So/Windows/CreateDataWindows11VM.php
+++ src/VM/App/Service/Help/So/Windows/CreateDataWindows11VM.php
@@ -35,102 +35,21 @@ final class CreateDataWindows11VM implements IBuildVMData
 
     use GFunctions;
 
-    private string $nodeName;
-    private int $vmId;
-    private ?int $vmCpuCores;
-    private ?string $vmName;
-    private ?int $vmNetId;
-    private ?string $vmNetModel;
-    private ?string $vmNetBridge;
-    private ?int $vmNetFirewall;
-    private ?bool $vmOnBoot;
-    private ?string $vmScsiHw;
-    private ?string $vmDiskType;
-    private ?int    $vmDiskId;
-    private ?string $vmDiskStorage;
-    private ?string $vmDiskDiscard;
-    private ?string $vmDiskCache;
-    private ?string $vmDiskImportFrom;
-    private ?string $vmTags;
-    private ?int    $vmCloudInitIdeId;
-    private ?string $vmCloudInitStorage;
-    private ?string $vmBootOrder;
-    private ?int $vmAgent;
-    private ?int    $vmNetNetId;
-    private ?string $vmNetIp;
-    private ?string $vmNetGw;
-    private ?string $vmOsUserName;
-    private ?string $vmOsPassword;
-    private ?string $vmCpuType;
-    private ?int $vmMemory = null;
-    private ?int $vmMemoryBallon = null;
-    private ?string $vmOsType = null;
-    private ?string $vmBios = null;
-    private ?string $vmMachinePc = null;
-    private ?string $vmEfiStorage = null;
-    private ?int $vmEfiKey = null;
-    private ?string $efidisckNvme = null;
-    private ?string $efidisckEnrroled = null;
-    private ?string $tpmstateNvme = null;
-    private ?string $tpmstateVersion = null;
-
-
-
     public function __construct(
-                                    string  $nodeName, int $vmId, ?int $vmCpuCores, ?string $vmName, ?int $vmNetId,
-                                    ?string $vmNetModel, ?string $vmNetBridge, ?int $vmNetFirewall, ?bool $vmOnBoot,
-                                    ?string $vmScsiHw, ?string $vmDiskType, ?int    $vmDiskId, ?string $vmDiskStorage,
-                                    ?string $vmDiskDiscard, ?string $vmDiskCache, ?string $vmDiskImportFrom, ?string $vmTags,
-                                    ?int    $vmCloudInitIdeId, ?string $vmCloudInitStorage, ?string $vmBootOrder, ?int $vmAgent,
-                                    ?int    $vmNetNetId, ?string $vmNetIp, ?string $vmNetGw, ?string $vmOsUserName,
-                                    ?string $vmOsPassword, ?string $vmCpuType, ?int $vmMemory = null, ?int $vmMemoryBallon = null,
-                                    ?string $vmOsType = null,?string $vmBios = null,?string $vmMachinePc = null,
-                                    ?string $vmEfiStorage = null, ?int $vmEfiKey = null,
-                                    ?string $efidisckNvme = null, ?string $efidisckEnrroled = null,
-                                    ?string $tpmstateNvme = null, ?string $tpmstateVersion = null
+                                    private string  $nodeName, private int $vmId, private ?int $vmCpuCores, private ?string $vmName, private ?int $vmNetId,// NOSONAR
+                                    private ?string $vmNetModel, private ?string $vmNetBridge, private ?int $vmNetFirewall, private ?bool $vmOnBoot,// NOSONAR
+                                    private ?string $vmScsiHw, private ?string $vmDiskType, private ?int    $vmDiskId, private ?string $vmDiskStorage,// NOSONAR
+                                    private ?string $vmDiskDiscard, private ?string $vmDiskCache, private ?string $vmDiskImportFrom, private ?string $vmTags,// NOSONAR
+                                    private ?int    $vmCloudInitIdeId, private ?string $vmCloudInitStorage, private ?string $vmBootOrder, private ?int $vmAgent,// NOSONAR
+                                    private ?int    $vmNetNetId, private ?string $vmNetIp, private ?string $vmNetGw, private ?string $vmOsUserName,// NOSONAR
+                                    private ?string $vmOsPassword, private ?string $vmCpuType, private ?int $vmMemory = null, private ?int $vmMemoryBallon = null,// NOSONAR
+                                    private ?string $vmOsType = null,private ?string $vmBios = null,private ?string $vmMachinePc = null,// NOSONAR
+                                    private ?string $vmEfiStorage = null, private ?int $vmEfiKey = null,// NOSONAR
+                                    private ?string $efidisckNvme = null, private ?string $efidisckEnrroled = null,// NOSONAR
+                                    private ?string $tpmstateNvme = null, private ?string $tpmstateVersion = null// NOSONAR
                                 )
 
     {
-        $this->nodeName = $nodeName;
-        $this->vmId = $vmId;
-        $this->vmCpuCores = $vmCpuCores;
-        $this->vmName = $vmName;
-        $this->vmNetId = $vmNetId;
-        $this->vmNetModel = $vmNetModel;
-        $this->vmNetBridge = $vmNetBridge;
-        $this->vmNetFirewall = $vmNetFirewall;
-        $this->vmOnBoot = $vmOnBoot;
-        $this->vmScsiHw = $vmScsiHw;
-        $this->vmDiskType = $vmDiskType;
-        $this->vmDiskId = $vmDiskId;
-        $this->vmDiskStorage = $vmDiskStorage;
-        $this->vmDiskDiscard = $vmDiskDiscard;
-        $this->vmDiskCache = $vmDiskCache;
-        $this->vmDiskImportFrom = $vmDiskImportFrom;
-        $this->vmTags = $vmTags;
-        $this->vmCloudInitIdeId = $vmCloudInitIdeId;
-        $this->vmCloudInitStorage = $vmCloudInitStorage;
-        $this->vmBootOrder = $vmBootOrder;
-        $this->vmAgent = $vmAgent;
-        $this->vmNetNetId = $vmNetNetId;
-        $this->vmNetIp = $vmNetIp;
-        $this->vmNetGw = $vmNetGw;
-        $this->vmOsUserName = $vmOsUserName;
-        $this->vmOsPassword = $vmOsPassword;
-        $this->vmCpuType = $vmCpuType;
-        $this->vmMemory = $vmMemory;
-        $this->vmMemoryBallon = $vmMemoryBallon;
-        $this->vmOsType = $vmOsType;
-        $this->vmBios = $vmBios;
-        $this->vmMachinePc = $vmMachinePc;
-        $this->vmEfiStorage = $vmEfiStorage;
-        $this->vmEfiKey = $vmEfiKey;
-        $this->efidisckNvme = $efidisckNvme;
-        $this->efidisckEnrroled = $efidisckEnrroled;
-        $this->tpmstateNvme = $tpmstateNvme;
-        $this->tpmstateVersion = $tpmstateVersion;
-
-        
         
     }
     
@@ -140,13 +59,16 @@ final class CreateDataWindows11VM implements IBuildVMData
         $net= new NetModel($this->vmNetId, $this->vmNetModel, $this->vmNetBridge, $this->vmNetFirewall);
             
         $scsi= null;
-        if (strtolower($this->vmDiskType) == strtolower(DiskTypePVE::SCSI)) $scsi = new ScsiModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        if (strtolower($this->vmDiskType) == strtolower(DiskTypePVE::SCSI))
+        {
+            $scsi = new ScsiModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        }
         
-        $ide= new IdeModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        $ide= new IdeModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom ); // NOSONAR
 
-        $sata=new SataModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+        $sata=new SataModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom ); // NOSONAR
 
-       $virtio= new VirtioModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom );
+       $virtio= new VirtioModel($this->vmDiskId, $this->vmDiskStorage, $this->vmDiskDiscard, $this->vmDiskCache, $this->vmDiskImportFrom ); // NOSONAR
 
 
         
@@ -155,7 +77,7 @@ final class CreateDataWindows11VM implements IBuildVMData
 
         $cpu = new CpuModel($this->vmCpuType, $this->vmCpuCores, $this->vmMemory, $this->vmMemoryBallon);
 
-        $efi= !is_null($this->vmEfiKey)? new EfiModel($this->vmDiskStorage, $this->vmEfiKey) : null;
+        $efi= !is_null($this->vmEfiKey)? new EfiModel($this->vmDiskStorage, $this->vmEfiKey) : null; // NOSONAR
 
         $machinePc= new MachineModel($this->vmMachinePc, null);
 
